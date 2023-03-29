40ee67381bad90857ad052376b779cfacc6ca3bb
Franklin Souza <ffrancadesouza@gmail.com>
2023-03-29 16:09:18 -0300

    agora itens de lista são criados e posicionados um abaixo do outro ao invés de substituir o valor do item

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,14 +1,18 @@
 const CriarTarefa = (evento) => {
 	evento.preventDefault();
+	const lista = document.querySelector("[data-list]");
 	const input = document.querySelector("[data-form-input]");
 	const inputValue = input.value;
 	console.log(inputValue);
 	input.value = "";
 
-	const tarefa = document.querySelector("[data-task]");
 	const conteudo = `<p class="content">${inputValue}</p>`;
+	const tarefa = document.createElement("li");
 
 	tarefa.innerHTML = conteudo;
+	tarefa.classList.add("task");
+	lista.appendChild(tarefa);
+
 	input.value = "";
 };
 
